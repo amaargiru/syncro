814c74adfee546803d71d036db32914a62795255
amaargiru <war4one@gmail.com>
2021-09-25 19:36:15 +0500

    Update WPF app

--- Code/WpfApp/MainWindow.xaml.cs
+++ Code/WpfApp/MainWindow.xaml.cs
@@ -1,24 +1,53 @@
 ﻿using Ookii.Dialogs.Wpf;
+using SyncroCore;
+using System.IO;
 using System.Windows;
 
 namespace WpfApp;
 
 public partial class MainWindow : Window
 {
+    private string primaryDirectory;
+    private string secondaryDirectory;
+
     public MainWindow()
     {
         InitializeComponent();
+        primaryDirectory = secondaryDirectory = "";
     }
 
     private void OpenPrimaryButton_Click(object sender, RoutedEventArgs e)
     {
         VistaFolderBrowserDialog dialog = new();
-        dialog.Description = "Please select a folder.";
+        dialog.Description = "Please select a primary folder";
         dialog.UseDescriptionForTitle = true;
 
         if ((bool)dialog.ShowDialog(this))
         {
-            MessageBox.Show(this, "The selected folder is: " + dialog.SelectedPath, "Sample folder browser dialog");
+            primaryDirectory = dialog.SelectedPath;
+            OpenPrimaryTextBox.Text = primaryDirectory;
+        }
+    }
+
+    private void OpenSecondaryButton_Click(object sender, RoutedEventArgs e)
+    {
+        VistaFolderBrowserDialog dialog = new();
+        dialog.Description = "Please select a secondary folder";
+        dialog.UseDescriptionForTitle = true;
+
+        if ((bool)dialog.ShowDialog(this))
+        {
+            secondaryDirectory = dialog.SelectedPath;
+            OpenSecondaryTextBox.Text = secondaryDirectory;
+        }
+    }
+
+    private void CompareButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (Directory.Exists(primaryDirectory) && Directory.Exists(secondaryDirectory))
+        {
+            Synchronize synchronize = new();
+            var synchInfo = synchronize.PrepareMirror(primaryDirectory, secondaryDirectory);
         }
     }
 }
