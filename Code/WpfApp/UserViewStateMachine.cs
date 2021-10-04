f57d8e8a7bf7e271fa6fe410bc223ae52f869ad6
amaargiru <war4one@gmail.com>
2021-10-04 19:09:27 +0500

    Add draft of finite state machine (FSM) Мне достаточно неожиданно пришел оффер на программиста C++/Python, так что изучение C# пока откладывается...

---
+++ Code/WpfApp/UserViewStateMachine.cs
@@ -0,0 +1,33 @@
+﻿using Stateless;
+
+namespace WpfApp
+{
+    internal class UserViewStateMachine
+    {
+        enum Trigger
+        {
+            PrimaryEntered,
+            SecondaryEntered,
+            PrimaryNotEntered,
+            SecondaryNotEntered,
+            BothEntered,
+            TryToCompare,
+            DoneCompare,
+            TryToSyncronize
+        }
+
+        enum State
+        {
+            EnterPrimary,
+            EnterSecondary,
+            CheckPrimary,
+            CheckSecondary,
+            ReadyForCompare,
+            Compare,
+            Compared,
+            Syncronized
+        }
+
+        StateMachine<State, Trigger> _machine;
+    }
+}
