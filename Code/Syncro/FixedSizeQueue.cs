491f507c036654fdea4c4ca0384f4f2db16f8344
amaargiru <war4one@gmail.com>
2022-03-20 16:49:17 +0500

    Add FixedSizeQueue for future limiting console log depth

---
+++ Code/Syncro/FixedSizeQueue.cs
@@ -0,0 +1,18 @@
+﻿namespace Syncro;
+
+public class FixedSizeQueue<T>
+{
+    Queue<T> q = new();
+    private object lockObject = new();
+
+    public int Limit { get; set; }
+    public void Enqueue(T obj)
+    {
+        q.Enqueue(obj);
+        lock (lockObject)
+        {
+            T overflow;
+            while (q.Count > Limit && q.TryDequeue(out overflow)) ;
+        }
+    }
+}
--- Code/Syncro/Syncro.cs
+++ Code/Syncro/Syncro.cs
@@ -3,7 +3,14 @@ using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
 using System.Threading.Channels;
 
-Log.Logger = new LoggerConfiguration()
+namespace Syncro;
+
+internal static class ConsoleSyncro
+{
+    private static async Task Main()
+    {
+
+        Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console(
          theme: AnsiConsoleTheme.Code,
@@ -14,58 +21,66 @@ Log.Logger = new LoggerConfiguration()
         rollOnFileSizeLimit: true)
     .CreateLogger();
 
-Log.Debug("Run Syncro...");
+        Log.Debug("Run Syncro...");
 
-Channel<string> channel = Channel.CreateUnbounded<string>();
-Synchronize synchronize = new(channel);
-SynchInfo synchInfo;
+        var logLines = 4;
 
-var primaryDirectory = @"D:\Polygon\Example of two diff dirs 2\DirA";
-var secondaryDirectory = @"D:\Polygon\Example of two diff dirs 2\DirB";
+        Channel<string> channel = Channel.CreateUnbounded<string>();
+        Synchronize synchronize = new(channel);
+        SynchInfo synchInfo;
 
-if (!Directory.Exists(primaryDirectory))
-    Console.WriteLine($"Primary directory {primaryDirectory} doesn't exist");
-else if (!Directory.Exists(secondaryDirectory))
-    Console.WriteLine($"Secondary directory {secondaryDirectory} doesn't exist");
-else
-{
-    if (!Path.IsPathFullyQualified(primaryDirectory))
-    {
-        primaryDirectory = Path.GetFullPath(primaryDirectory);
-    }
+        var primaryDirectory = @"D:\Polygon\Example of two diff dirs 2\DirA";
+        var secondaryDirectory = @"D:\Polygon\Example of two diff dirs 2\DirB";
 
-    if (!Path.IsPathFullyQualified(secondaryDirectory))
-    {
-        secondaryDirectory = Path.GetFullPath(secondaryDirectory);
-    }
-
-    synchInfo = synchronize.PrepareMirror(primaryDirectory, secondaryDirectory);
-
-    var totalItemsToDo = synchInfo.Count();
-    Log.Debug($"Total items = {totalItemsToDo}");
-
-    var a = Console.GetCursorPosition();
-    Console.SetCursorPosition(0, a.Top);
-
-    MessageReaderAsync(channel);
-
-    await synchronize.DeleteSecondaryFilesAsync(secondaryDirectory, synchInfo);
-    await synchronize.DeleteSecondaryDirectories(secondaryDirectory, synchInfo);
-    await synchronize.CreateSecondaryDirectories(secondaryDirectory, synchInfo);
-    await synchronize.CopyFilesFromPrimaryToSecondary(primaryDirectory, secondaryDirectory, synchInfo);
-
-    Log.Debug("All done.");
-}
-
-Console.Read();
-
-static async Task MessageReaderAsync(Channel<string> channel)
-{
-    while (await channel.Reader.WaitToReadAsync())
-    {
-        if (channel.Reader.TryRead(out var msg))
+        if (!Directory.Exists(primaryDirectory))
+            Console.WriteLine($"Primary directory {primaryDirectory} doesn't exist");
+        else if (!Directory.Exists(secondaryDirectory))
+            Console.WriteLine($"Secondary directory {secondaryDirectory} doesn't exist");
+        else
         {
-            Log.Debug(msg);
+            if (!Path.IsPathFullyQualified(primaryDirectory))
+            {
+                primaryDirectory = Path.GetFullPath(primaryDirectory);
+            }
+
+            if (!Path.IsPathFullyQualified(secondaryDirectory))
+            {
+                secondaryDirectory = Path.GetFullPath(secondaryDirectory);
+            }
+
+            synchInfo = synchronize.PrepareMirror(primaryDirectory, secondaryDirectory);
+
+            var totalItemsToDo = synchInfo.Count();
+            Log.Debug($"Total items = {totalItemsToDo}");
+
+            var currentCursorPosition = Console.GetCursorPosition();
+            // Console.SetCursorPosition(0, a.Top);
+
+            MessageLoggerAsync(channel, logLines, currentCursorPosition.Top);
+
+            await synchronize.DeleteSecondaryFilesAsync(secondaryDirectory, synchInfo);
+            await synchronize.DeleteSecondaryDirectories(secondaryDirectory, synchInfo);
+            await synchronize.CreateSecondaryDirectories(secondaryDirectory, synchInfo);
+            await synchronize.CopyFilesFromPrimaryToSecondary(primaryDirectory, secondaryDirectory, synchInfo);
+
+            Log.Debug("All done.");
+
+            Console.Read();
+        }
+    }
+
+    static async Task MessageLoggerAsync(Channel<string> channel, int logLines, int currentCursorY)
+    {
+        FixedSizeQueue<string> log_messages = new();
+        log_messages.Limit = logLines;
+
+        while (await channel.Reader.WaitToReadAsync())
+        {
+            if (channel.Reader.TryRead(out var msg))
+            {
+                log_messages.Enqueue(msg);
+                Log.Debug(msg);
+            }
         }
     }
 }
